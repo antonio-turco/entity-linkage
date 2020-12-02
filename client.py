c47b488842f03a1760aaad41f26411e74f18485f
Antonio Turco <truock@gmail.com>
2020-12-02 13:34:42 +0100

    :recycle: refactor intersection logic and removes labels from every item
    Now labels are store only one time for page

--- client.py
+++ client.py
@@ -24,12 +24,10 @@ leaves_sites = []
 #estraggo termini foglia per ogni link
 for site in sites:
 	site_leaves = []
-	for page in site:
-		log("extracting from: " + page[1])
-		extracted_leaves = wrapper.get_all_leaves(page[1])
-		#ad ogni termine ci associo la pagina sorgente
-		labeled_leaves = wrapper.assoc_key_to_leaves(page[0], extracted_leaves)
-		site_leaves.append(labeled_leaves)
+	for label, page in site:
+		log("extracting from: " + page)
+		extracted_leaves = wrapper.get_all_leaves(page)
+		site_leaves.append( (label, extracted_leaves) )
 		log("leaves extracted")
 	leaves_sites.append(site_leaves)
 
@@ -41,21 +39,6 @@ for leaves_site in leaves_sites:
 	unique_leaves_sites.append(unique_leaves_site)
 
 #associo le pagine inter-sito in base alla similiarità
-
-'''
-
-siteA
-siteB
-
-for page1 in siteA
-	for page2 in siteB
-		similiarity(site1, site2)
-		
-
-similiarity(site1, site2):
-	len(intersection([site1, site2]))/(len(site1) + len(site2))
-
-'''
 def similiarity(page1, page2):
 	intersection = wrapper.intersection([page1, page2])
 	return len(intersection)/(len(page1) + len(page2))
@@ -63,17 +46,15 @@ def similiarity(page1, page2):
 siteA = unique_leaves_sites[0]
 siteB = unique_leaves_sites[1]
 
-for page1 in siteA:
+for label1, page1 in siteA:
 	max_sim = 0
-	max_key1 = 0
-	max_key2 = 0
-	for page2 in siteB:
-		key1 = page1[0][0]
-		key2 = page2[0][0]
+	max_label1 = 0
+	max_label2 = 0
+	for label2, page2 in siteB:
 		sim = similiarity(page1, page2)
 		if sim > max_sim:
 			max_sim = sim
-			max_key1 = key1
-			max_key2 = key2
-	log("page " + str(max_key1) + " is associated with " + str(max_key2))
+			max_label1 = label1
+			max_label2 = label2
+	log("page " + str(max_label1) + " is associated with " + str(max_label2))
 
--- wrapper.py
+++ wrapper.py
@@ -4,40 +4,37 @@ from lxml import html
 class Wrapper:
 
 	def get_all_leaves(self, path):
-		#page = requests.get(url)
-		#print(page.text)
 		file = open(path, "r")
 		page = file.read()
 		file.close()
 		dom_tree = html.fromstring(page)
 		all_leaves = dom_tree.xpath("//body//*[text()[not(normalize-space()='')]][not(self::script or self::style or self::meta or self::noscript)]/text()")
-		#all_leaves = dom_tree.xpath("//*[not(*)][not(self::script or self::style or self::meta or self::noscript)]/text()")
-		#all_leaves = dom_tree.xpath("//body//*[text()[not(normalize-space()='')]]/text()")
 		return all_leaves
 
 	def assoc_key_to_leaves(self, key, leaves):
 		return list(map(lambda leaf: (key, leaf), leaves))
 
-	def remove_key_from_leaves(self, leaves):
+	def remove_label_from_leaves(self, leaves):
 		return list(map(lambda key_leaf: key_leaf[1], leaves))
 
 	def intersection(self, site_leaves):
-		intersection = set(self.remove_key_from_leaves(site_leaves[0]))
+		intersection = set(site_leaves[0])
 		for site in site_leaves:
-			leaves_set = set(self.remove_key_from_leaves(site))
+			leaves_set = set(site)
 			intersection = intersection & leaves_set
 		return intersection
 
 	def get_unique_leaves(self, site_leaves, intersection):
 		unique_site_leaves = []
-		for page in site_leaves:
+		for label, page in site_leaves:
 			unique_page_leaves = []
-			for key, element in page:
+			for element in page:
 				if element not in intersection:
-					unique_page_leaves.append( (key, element) )
-			unique_site_leaves.append(unique_page_leaves)
+					unique_page_leaves.append(element)
+			unique_site_leaves.append( (label, unique_page_leaves) )
 		return unique_site_leaves
 
 	def diff(self, site_leaves):
-		intersection = self.intersection(site_leaves)
+		site_leaves_pages = self.remove_label_from_leaves(site_leaves)
+		intersection = self.intersection(site_leaves_pages)
 		return self.get_unique_leaves(site_leaves, intersection)

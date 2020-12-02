1199e19e4dc0137c7fedde0fafbc67bd3312b93f
Antonio Turco <truock@gmail.com>
2020-12-02 11:28:57 +0100

    :truck: defines a better organized workspace

--- client.py
+++ client.py
@@ -7,15 +7,15 @@ def log(message):
 wrapper = Wrapper()
 
 siteA = [
-	(0, "john-wall-rotoworld.html"),
-	(1, "danilo-gallinari-rotoworld.html"),
-	(2, "luguentz-dort-rotoworld.html"),
+	(0, "dataset/john-wall-rotoworld.html"),
+	(1, "dataset/luguentz-dort-rotoworld.html"),
+	(2, "dataset/danilo-gallinari-rotoworld.html"),
 ]
 
 siteB = [
-	(3, "danilo-gallinari-nba.html"),
-	(4, "john-wall-nba.html"),
-	(5, "luguentz-dort-nba.html"),
+	(3, "dataset/danilo-gallinari-nba.html"),
+	(4, "dataset/john-wall-nba.html"),
+	(5, "dataset/luguentz-dort-nba.html"),
 ]
 
 sites = [siteA, siteB]
